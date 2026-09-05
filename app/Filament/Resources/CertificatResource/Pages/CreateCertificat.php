615f2d9a8b705d79f3b193d46d2b49fe450c1b58
Remy A. Agbokpe <205567368+hackwithoutbarriers@users.noreply.github.com>
2026-09-05 01:25:19 +0000

    Add files via upload

--- app/Filament/Resources/CertificatResource/Pages/CreateCertificat.php
+++ app/Filament/Resources/CertificatResource/Pages/CreateCertificat.php
@@ -5,6 +5,10 @@ namespace App\Filament\Resources\CertificatResource\Pages;
 use App\Filament\Resources\CertificatResource;
 use Filament\Resources\Pages\CreateRecord;
 
+/**
+ * Kept for compatibility with previously cached Filament panel routes.
+ * Certificate creation is now exposed through AttestationResource.
+ */
 class CreateCertificat extends CreateRecord
 {
     protected static string $resource = CertificatResource::class;
