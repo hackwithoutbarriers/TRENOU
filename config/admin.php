0ef0224e4aaf1bbe38fb7a424fcdc6f7e0a351b3
Remy A. Agbokpe <205567368+hackwithoutbarriers@users.noreply.github.com>
2026-09-07 23:02:36 +0000

    Delete config directory

--- config/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'name' => env('ADMIN_NAME', 'Administrateur TRENOU'),
-    'email' => env('ADMIN_EMAIL'),
-    'password' => env('ADMIN_PASSWORD'),
-    'force_password_reset' => (bool) env('ADMIN_FORCE_PASSWORD_RESET', false),
-];
